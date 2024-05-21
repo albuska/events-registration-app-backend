321bc30543042ac260c635945f635bcca2ad492d
Albina Anistratenko <albuska2013@gmail.com>
2024-05-21 16:10:52 +0300

    fixed listEvents: add sorted

--- dist/controllers/events/getListEvents.js
+++ dist/controllers/events/getListEvents.js
@@ -3,7 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const models_1 = require("../../models");
 const helpers_1 = require("../../helpers");
 const listEvents = async (req, res) => {
-    const result = await models_1.Event.find();
+    const { sortBy, sortOrder } = req.query;
+    const sortOptions = {};
+    if (typeof sortBy === "string" && typeof sortOrder === "string") {
+        sortOptions[sortBy] = sortOrder === "desc" ? -1 : 1;
+    }
+    const result = await models_1.Event.find().sort(sortOptions);
     res.status(200).json({
         result,
     });
--- src/controllers/events/getListEvents.ts
+++ src/controllers/events/getListEvents.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from "express";
 import { Event } from "../../models";
 import { ctrlWrapper } from "../../helpers";
+import { ParsedQs } from "qs";
 
 const listEvents = async (req: Request, res: Response) => {
-  const result = await Event.find();
+  const { sortBy, sortOrder } = req.query as {
+    sortBy: ParsedQs;
+    sortOrder: ParsedQs;
+  };
+  const sortOptions: { [key: string]: 1 | -1 } = {};
+
+  if (typeof sortBy === "string" && typeof sortOrder === "string") {
+    sortOptions[sortBy] = sortOrder === "desc" ? -1 : 1;
+  }
+
+  const result = await Event.find().sort(sortOptions);
 
   res.status(200).json({
     result,
