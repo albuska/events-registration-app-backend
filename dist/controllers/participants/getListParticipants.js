7829984706c82ecd01c6e171dfb72e801f27ab43
Albina Anistratenko <albuska2013@gmail.com>
2024-05-21 14:31:03 +0300

    add query in listParticipants

--- dist/controllers/participants/getListParticipants.js
+++ dist/controllers/participants/getListParticipants.js
@@ -3,10 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const models_1 = require("../../models");
 const helpers_1 = require("../../helpers");
 const listParticipants = async (req, res) => {
-    const result = await models_1.Participant.find({ event: req.params.id });
-    res.status(200).json({
-        result,
-    });
+    const { id } = req.params;
+    const { query } = req.query;
+    let filter = { event: id };
+    if (query) {
+        filter = {
+            ...filter,
+            $or: [
+                { fullName: { $regex: new RegExp(query, "i") } },
+                { email: { $regex: new RegExp(query, "i") } },
+            ],
+        };
+    }
+    try {
+        const result = await models_1.Participant.find(filter);
+        res.status(200).json({ result });
+    }
+    catch (error) {
+        console.error("Error listing participants:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 };
 exports.default = {
     listParticipants: (0, helpers_1.ctrlWrapper)(listParticipants),
--- src/controllers/participants/getListParticipants.ts
+++ src/controllers/participants/getListParticipants.ts
@@ -3,11 +3,28 @@ import { Participant } from "../../models";
 import { ctrlWrapper } from "../../helpers";
 
 const listParticipants = async (req: Request, res: Response) => {
-  const result = await Participant.find({ event: req.params.id });
+  const { id } = req.params;
+  const { query } = req.query;
 
-  res.status(200).json({
-    result,
-  });
+  let filter: any = { event: id };
+
+  if (query) {
+    filter = {
+      ...filter,
+      $or: [
+        { fullName: { $regex: new RegExp(query as string, "i") } },
+        { email: { $regex: new RegExp(query as string, "i") } },
+      ],
+    };
+  }
+
+  try {
+    const result = await Participant.find(filter);
+    res.status(200).json({ result });
+  } catch (error) {
+    console.error("Error listing participants:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export default {
